f8007863ada65832419595724e3ed246fd568374
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-01-31 19:33:56 +0100

    Mejora 3

--- src/main/java/liceosorolla/Array.java
+++ src/main/java/liceosorolla/Array.java
@@ -4,6 +4,18 @@ public class Array {
 
 	public static void main(String[] args) {
 		int[]numeros={1,25,5,15,10};
-
+		System.out.println("La mediana es "+calcularMediana(numeros));
 	}
+	
+	  private static double calcularMediana(int[]numeros){
+		  int med=0;
+			java.util.Arrays.sort(numeros);
+			if(numeros.length%2!=0) {
+				med=numeros[numeros.length/2];
+			}else {
+				med=(numeros[numeros.length/2]+numeros[numeros.length/2-1])/2;
+			}
+			
+			return med;
+		}
 }
