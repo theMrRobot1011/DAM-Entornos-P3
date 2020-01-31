f679a74bda9a0c0d715b5cd110abab56aed0bed6
DAM_1 <DAM_1@PB09>
2020-01-31 20:49:35 +0100

    Mejora-2

--- src/main/java/liceosorolla/Array.java
+++ src/main/java/liceosorolla/Array.java
@@ -4,6 +4,16 @@ public class Array {
 
 	public static void main(String[] args) {
 		int[]numeros={1,25,5,15,10};
-
+		System.out.println("El numero minimo es "+minimo(numeros));
+	}
+	public static int minimo(int[]array) {
+		int min=array[0];
+		for(int i=0;i<array.length;i++) {
+			if(array[i]<min) {
+				min=array[i];
+			}
+		}
+		
+		return min;
 	}
 }
