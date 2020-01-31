0ab270fff13b620b8915f5e5ac907013691d7a9d
DAM_1 <DAM_1@PB07>
2020-01-31 20:44:25 +0100

    cambio

--- src/main/java/liceosorolla/Numero.java
+++ src/main/java/liceosorolla/Numero.java
@@ -1,11 +1,18 @@
 package main.java.liceosorolla;
 
 public class Numero {
-
+	
+	public static int factorial(int numero) {
+		int factorial=1;
+		for(int i=1;i<=numero;i++) {
+			factorial=factorial*i;
+		}
+		return factorial;
+	}
+	
 	public static void main(String[] args) {
-	 
 		int numero= 5;
-
+		System.out.println("El factorial es "+factorial(numero));
 	}
 
 }
