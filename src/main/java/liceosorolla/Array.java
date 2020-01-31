e4f07aa295eb254fa3b8032893458db671c42649
DAM_1 <DAM_1@PB07>
2020-01-31 20:34:24 +0100

    cambio

--- src/main/java/liceosorolla/Array.java
+++ src/main/java/liceosorolla/Array.java
@@ -1,9 +1,25 @@
 package main.java.liceosorolla;
 
 public class Array {
-
-	public static void main(String[] args) {
+	
+	public static int calculaMximo( int[] letras)  throws Exception {
+		int maximo=0;
+		for(int i =0;i<=letras.length-1;i++) {
+			if(i==0) {
+				maximo= letras[i];
+			}else if(maximo<letras[i]){
+				maximo= letras[i];
+			}
+		}
+		
+		return maximo;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int maximo=0;
 		int[]numeros={1,25,5,15,10};
-
+		
+		maximo = calculaMximo(numeros);
+		System.out.println("El numero mas alto es "+maximo);
 	}
 }
